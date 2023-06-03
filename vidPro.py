75e4fa9a1c60d1b27db494e3cc79ab9dbd8ea4a0
Ukiyyy <uros.mravljak@student.um.si>
2023-06-03 17:24:48 +0200

    last version

--- vidPro.py
+++ vidPro.py
@@ -1,44 +1,69 @@
 import numpy as np
 import cv2
 from skimage.feature import local_binary_pattern
+import os
+import pymongo
+from bson.binary import Binary
+from os import listdir
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+myclient = pymongo.MongoClient("mongodb+srv://vidPro:vidPro@vidpro.wsmmizs.mongodb.net/")
+mydb = myclient["vidPro"]
+mycol = mydb["images"]
 
 
-# Funkcija za izračun LBP značilnic
-def calculate_lbp(image):
-    # Pretvorba slike v sivinsko območje
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Izračun LBP značilnic
-    lbp = local_binary_pattern(gray, 8, 1, method='uniform')
-    hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, 10), range=(0, 10))
-    hist = hist.astype("float")
-    hist /= (hist.sum() + 1e-7)
-    return hist
+# Funkcija, ki shrani sliko v bazo. Parameter "team" je bool, ce je iz nase mikroskupine al ne.
+def shraniSliko(img, team):
+    slika_bin = cv2.imencode('.png', img)[1].tobytes()
 
-# Branje in predobdelava podatkov
-def preprocess_data():
-    # Branje slik in pridobivanje LBP značilnic ter oznak
-    images = []
-    labels = []
-    for i in range(num_samples):
-        image = cv2.imread(f"path/to/images/{i}.jpg")
-        faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            face_image = image[y:y+h, x:x+w]
-            lbp_features = calculate_lbp(face_image)
-            images.append(lbp_features)
-            labels.append(class_label)  # Nastavite ustrezno oznako glede na razpoznavni razred
-    return images, labels
+    mydict = {
+        "image": Binary(slika_bin),
+        "isTeam": team
+    }
 
-video_capture = cv2.VideoCapture(0)
+    x = mycol.insert_one(mydict)
 
-# Nalaganje predhodno naučenega modela za zaznavanje obrazov
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    if x.inserted_id:
+        print("Slika uspešno shranjena v MongoDB.")
+    else:
+        print("Prišlo je do napake pri shranjevanju slike v MongoDB.")
 
-while True:
-    # Branje sličice iz video posnetka
+
+# Funkcija, ki vrne vse slike iz podatkovne baze
+def dobiSlike():
+    documents = mycol.find()
+
+    # Seznam za shranjevanje slik
+    slike = []
+
+    # Iteracija čez dokumente
+    for document in documents:
+        # Pridobite binarne podatke slike iz dokumenta
+        image_data = document["image"]
+
+        # Pretvorite binarne podatke v numpy array
+        nparr = np.frombuffer(image_data, np.uint8)
+
+        # Dekodirajte numpy array v sliko z uporabo OpenCV
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        # Dodajte sliko v seznam
+        slike.append(image)
+
+    return slike
+
+
+def zaznajObrazKamera():
+    video_capture = cv2.VideoCapture(0)
+
+    # Nalaganje predhodno naučenega modela za zaznavanje obrazov
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Branje prve sličice iz video posnetka
     ret, frame = video_capture.read()
 
     # Pretvorba sličice v sivinsko sliko (za zaznavanje obrazov je potrebna sivinska slika)
@@ -47,17 +72,110 @@ while True:
     # Zaznavanje obrazov v sličici
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Označevanje zaznanih obrazov s pravokotniki
+    # Izrezovanje in prikazovanje zaznanih obrazov
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Izrez območja z obrazom iz sličice
+        face_image = frame[y:y + h, x:x + w]
 
-    # Prikaži video posnetek z označenimi obrazi
-    cv2.imshow('Video', frame)
+        # Prikaz izrezanega obraza
+        # cv2.imshow('Face', face_image)
 
-    # Prekini zanko ob pritisku na tipko 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Počakajte, da uporabnik pritisne tipko
+    # cv2.waitKey(0)
 
-# Sprosti vir zajema video posnetka in zapri okno
-video_capture.release()
+    # Sprosti vir zajema video posnetka in zapri okno
+    return face_image
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+def zaznajObrazSlika(path):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Naloži sliko
+    image = cv2.imread(path)
+
+    # Pretvori sliko v sivinsko sliko
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Zaznaj obraz na sliki
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Označi obraz z okvirjem
+    for (x, y, w, h) in faces:
+        face_image = image[y:y + h, x:x + w]
+
+    # Prikaži sliko z označenimi obrazom
+    return face_image
+    cv2.destroyAllWindows()
+
+
+def get_piksel(img, center, x, y):
+    new_value = 0
+    try:
+        if img[x][y] >= center:
+            new_value = 1
+
+    except:
+        pass
+
+    return new_value
+
+
+def lbp_zracunan_piksel(img, x, y):
+    center = img[x][y]
+    value = []
+
+    value.append(get_piksel(img, center, x - 1, y - 1))
+    value.append(get_piksel(img, center, x - 1, y))
+    value.append(get_piksel(img, center, x - 1, y + 1))
+    value.append(get_piksel(img, center, x, y + 1))
+    value.append(get_piksel(img, center, x + 1, y + 1))
+    value.append(get_piksel(img, center, x + 1, y))
+    value.append(get_piksel(img, center, x + 1, y - 1))
+    value.append(get_piksel(img, center, x, y - 1))
+
+    t = [1, 2, 4, 8, 16, 32, 64, 128]
+    values = 0
+
+    for i in range(len(value)):
+        values += value[i] * t[i]
+
+    return values
+
+
+# Funkcija, ki izloci iz vseh slik iz baze znacilnice(lbp)
+def izlocanjeZnacilnic():
+    slike = dobiSlike()
+    imageslbp = []
+    labels = []
+    for slika in slike:
+        height, width, _ = slika.shape
+        face_image = cv2.cvtColor(slika, cv2.COLOR_BGR2GRAY)
+        img_lbp = np.zeros((height, width), np.uint8)
+        for i in range(0, height):
+            for j in range(0, width):
+                img_lbp[i, j] = lbp_zracunan_piksel(face_image, i, j)
+        imageslbp.append(img_lbp)
+
+    return imageslbp
+
+
+# Za shranjevanje obrazov. Opcija 3 za testiranje, ce je slika v bazi
+option = int(input('Enter your choice: '))
+if option == 1:
+    print('Dodaj novo osebo preko kamere')
+    img = zaznajObrazKamera()
+    shraniSliko(img, 1)
+elif option == 2:
+    print('Dodaj novo osebo z sliko')
+    img = zaznajObrazSlika('lenna.png')
+    shraniSliko(img, 0)
+elif option == 3:
+    slike = dobiSlike()
+    cv2.imshow("Slika", slike[1])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+cv2.waitKey()
+
 cv2.destroyAllWindows()
